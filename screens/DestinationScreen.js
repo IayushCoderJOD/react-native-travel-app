6db7dd1146d93a775374fa6f51f48b9011b0073e
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-01-09 12:43:47 +0530

    done

--- screens/DestinationScreen.js
+++ screens/DestinationScreen.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   View,
   Text,
@@ -6,21 +7,21 @@ import {
   ScrollView,
   Platform,
 } from "react-native";
-import React, { useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import {
+  ChevronLeftIcon,
   ClockIcon,
   HeartIcon,
   MapPinIcon,
   SunIcon,
 } from "react-native-heroicons/solid";
 import { useNavigation } from "@react-navigation/native";
+import tw from "twrnc";
 import { theme } from "../theme";
 
 const ios = Platform.OS == "ios";
@@ -32,28 +33,28 @@ export default function DestinationScreen(props) {
   const [isFavourite, toggleFavourite] = useState(false);
 
   return (
-    <View className="bg-white flex-1">
-      {/* destination image */}
+    <View style={tw`bg-white flex-1`}>
       <Image source={item.image} style={{ width: wp(100), height: hp(55) }} />
       <StatusBar style={"light"} />
 
-      {/* back button */}
       <SafeAreaView
-        className={
-          "flex-row justify-between items-center w-full absolute " + topMargin
-        }
+        style={tw`flex-row justify-between items-center w-full absolute ${topMargin}`}
       >
         <TouchableOpacity
           onPress={() => navigation.goBack()}
-          className="p-2 rounded-full ml-4"
-          style={{ backgroundColor: "rgba(255,255,255,0.5)" }}
+          style={{
+            ...tw`p-2 rounded-full ml-4`,
+            backgroundColor: "rgba(255,255,255,0.5)",
+          }}
         >
           <ChevronLeftIcon size={wp(7)} strokeWidth={4} color="white" />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => toggleFavourite(!isFavourite)}
-          className="p-2 rounded-full mr-4"
-          style={{ backgroundColor: "rgba(255,255,255,0.5)" }}
+          style={{
+            ...tw`p-2 rounded-full mr-4`,
+            backgroundColor: "rgba(255,255,255,0.5)",
+          }}
         >
           <HeartIcon
             size={wp(7)}
@@ -63,80 +64,97 @@ export default function DestinationScreen(props) {
         </TouchableOpacity>
       </SafeAreaView>
 
-      {/* title & descritpion & booking button */}
       <View
-        style={{ borderTopLeftRadius: 40, borderTopRightRadius: 40 }}
-        className="px-5 flex flex-1 justify-between bg-white pt-8 -mt-14"
+        style={{
+          ...tw`px-5 flex flex-1 justify-between bg-white pt-8 -mt-14`,
+          borderTopLeftRadius: 40,
+          borderTopRightRadius: 40,
+        }}
       >
-        <ScrollView showsVerticalScrollIndicator={false} className="space-y-5">
-          <View className="flex-row justify-between items-start">
+        <ScrollView showsVerticalScrollIndicator={false} style={tw`space-y-5`}>
+          <View style={tw`flex-row justify-between items-start`}>
             <Text
-              style={{ fontSize: wp(7) }}
-              className="font-bold flex-1 text-neutral-700"
+              style={{
+                ...tw`font-bold flex-1 text-neutral-700`,
+                fontSize: wp(7),
+              }}
             >
               {item?.title}
             </Text>
             <Text
-              style={{ fontSize: wp(7), color: theme.text }}
-              className="font-semibold"
+              style={{ fontSize: wp(7), color: theme.text, fontWeight: "600" }}
             >
               $ {item?.price}
             </Text>
           </View>
           <Text
-            style={{ fontSize: wp(3.7) }}
-            className="text-neutral-700 tracking-wide mb-2"
+            style={{
+              ...tw`font-normal text-md mt-6 `,
+              color: "black",
+              letterSpacing: 0.5,
+              lineHeight: 22,
+            }}
           >
             {item?.longDescription}
           </Text>
-          <View className="flex-row justify-between mx-1">
-            <View className="flex-row space-x-2 items-start">
+          <View style={tw`flex-row justify-between mx-1 mt-20`}>
+            <View style={tw`flex-row space-x-2 items-start`}>
               <ClockIcon size={wp(7)} color="skyblue" />
-              <View className="flex space-y-2">
+              <View style={tw`flex space-y-2`}>
                 <Text
-                  style={{ fontSize: wp(4.5) }}
-                  className="font-bold text-neutral-700"
+                  style={{
+                    fontSize: wp(4.5),
+                    fontWeight: "bold",
+                    color: theme.text,
+                  }}
                 >
                   {item.duration}
                 </Text>
-                <Text className="text-neutral-600 tracking-wide">Duration</Text>
+                <Text style={tw`text-neutral-600 tracking-wide`}>Duration</Text>
               </View>
             </View>
-            <View className="flex-row space-x-2 items-start">
+            <View style={tw`flex-row space-x-2 items-start`}>
               <MapPinIcon size={wp(7)} color="#f87171" />
-              <View className="flex space-y-2">
+              <View style={tw`flex space-y-2`}>
                 <Text
-                  style={{ fontSize: wp(4.5) }}
-                  className="font-bold text-neutral-700"
+                  style={{
+                    fontSize: wp(4.5),
+                    fontWeight: "bold",
+                    color: theme.text,
+                  }}
                 >
                   {item.distance}
                 </Text>
-                <Text className="text-neutral-600 tracking-wide">Distance</Text>
+                <Text style={tw`text-neutral-600 tracking-wide`}>Distance</Text>
               </View>
             </View>
-            <View className="flex-row space-x-2 items-start">
+            <View style={tw`flex-row space-x-2 items-start`}>
               <SunIcon size={wp(7)} color="orange" />
-              <View className="flex space-y-2">
+              <View style={tw`flex space-y-2`}>
                 <Text
-                  style={{ fontSize: wp(4.5) }}
-                  className="font-bold text-neutral-700"
+                  style={{
+                    fontSize: wp(4.5),
+                    fontWeight: "bold",
+                    color: theme.text,
+                  }}
                 >
                   {item.weather}
                 </Text>
-                <Text className="text-neutral-600 tracking-wide">Sunny</Text>
+                <Text style={tw`text-neutral-600 tracking-wide`}>Sunny</Text>
               </View>
             </View>
           </View>
         </ScrollView>
         <TouchableOpacity
           style={{
-            backgroundColor: theme.bg(0.8),
+            ...tw`mb-6 mx-auto flex justify-center items-center rounded-full bg-${theme.bg(
+              0.8
+            )}`,
             height: wp(15),
             width: wp(50),
           }}
-          className="mb-6 mx-auto flex justify-center items-center rounded-full"
         >
-          <Text className="text-white font-bold" style={{ fontSize: wp(5.5) }}>
+          <Text style={{ ...tw`text-white font-bold`, fontSize: wp(5.5) }}>
             Book now
           </Text>
         </TouchableOpacity>
